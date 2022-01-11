94b0f079e362abb185d1d32a0f50454ffa0d9fde
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-12 04:53:26 +0900

    Fix ga import tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,22 +11,17 @@ export default class MyDocument extends Document {
           {GA_TRACKING_ID && (
             <>
               <Script
-                defer
                 src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
                 strategy="afterInteractive"
               />
-              <Script
-                defer
-                dangerouslySetInnerHTML={{
-                  __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}');
-            `,
-                }}
-                strategy="afterInteractive"
-              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', ${GA_TRACKING_ID});
+                `}
+              </Script>
             </>
           )}
         </Head>
