a8fd2dac97d94a494c135284b2c402e7b8ace853
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-12 01:16:57 +0900

    add style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 
+import { Layout } from "../components/layout";
 import __ from "../styles/normalize.css";
 import _ from "../styles/reset.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={`${_.content} ${__.content}`}>
-      <Component {...pageProps} />;
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
     </div>
   );
 }
