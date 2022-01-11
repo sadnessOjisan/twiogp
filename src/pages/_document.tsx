5ec2401006b417d71c0c4265a92b8c711c51d1b4
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-12 04:17:56 +0900

    set ga

---
+++ src/pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+import { GA_TRACKING_ID } from "../util/ga";
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          {GA_TRACKING_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+        `,
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
